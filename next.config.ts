a43a7f315ab3e7c73231551e845988cac17e2abd
Mrs@16122003 <miteshsonagra5576@gmail.com>
2025-09-15 10:59:09 +0530

    next js config fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.resolve.alias = {
